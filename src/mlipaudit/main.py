c1a414fd16c9d256ecee0b9362623f33376f2e62
Lucien Walewski <l.walewski@instadeep.com>
2025-07-24 09:46:04 +0200

    feat: add basic benchmark structure and conformer selection (#1)
    * feat: add basic benchmark structure
    
    * feat: add pre-commit config
    
    * chore: update lockfile
    
    * feat: update Benchmark class to have two methods
    
    * feat: update benchmark class to download data
    
    * feat: add small mol conformer selection (#2)
    
    * data: add wiggle 150 dataset
    
    * feat: update .gitignore for .DS_Store files
    
    * feat: update `Benchmark` download logic
    
    * feat: add conformer_selection.py
    
    * feat: add notebook and pytest
    
    * test: conformer selection
    
    * feat: add sklearn dependency
    
    * feat: add data preprocessing script
    
    * data: add preprocessed dataset
    
    * data: add preprocessed dataset
    
    * feat: finalizing full first end-to-end implementation (#3)
    
    * feat: remove data and scripts folders
    
    * feat: implement basic data loading code
    
    * refactor: fixing some issues in first benchmark
    
    * test: finish first easy test for first benchmark
    
    * feat: write benchmark results to disk
    
    * test: complete tests for first benchmark
    
    * feat: add argparse and option to use multiple models to main.py
    
    * refactor: small clean-up of first benchmark
    
    * docs: update README and main.py
    
    * feat: add streamlit app
    
    * feat: also store results in benchmark class
    
    * feat: refactor ui page for first benchmark, modify tests a bit
    
    * test: add test for I/O functinality
    
    * docs: add more license headers
    
    * refactor: every benchmark should just return a single result class and not a list of them
    
    * docs: specify zip naming convention
    
    * refactor: move main.py to src and remove 2nd for loop
    
    * feat: update main to be run as `mlipaudit`
    
    * feat: improved logging
    
    Update the loggging config so that INFO is the base level.
    
    * feat: add linting to ci
    
    * fix: conformer selection tests
    
    * ci: run pytests
    
    * ci: have a separate job for tests
    
    * test: add mock force field
    
    * test: update conformer selection tests
    
    * chore: delete model zip from tests
    
    * feat: only ignore top-level data
    
    * feat: add wiggle150 data test file
    
    ---------
    
    Co-authored-by: Lucien Walewski <l.walewski@instadeep.com>
    
    ---------
    
    Co-authored-by: Christoph Brunken <c.brunken@instadeep.com>
    
    ---------
    
    Co-authored-by: Christoph Brunken <c.brunken@instadeep.com>

---
+++ src/mlipaudit/main.py
@@ -0,0 +1,97 @@
+# Copyright 2025 InstaDeep Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+from argparse import ArgumentParser
+from pathlib import Path
+
+from mlip.models import Mace, Nequip, Visnet
+from mlip.models.mlip_network import MLIPNetwork
+from mlip.models.model_io import load_model_from_zip
+
+from mlipaudit.io import write_benchmark_results_to_disk
+from mlipaudit.small_molecule_conformer_selection import ConformerSelectionBenchmark
+
+logger = logging.getLogger("mlipaudit")
+
+BENCHMARKS = [ConformerSelectionBenchmark]
+
+
+def _parser() -> ArgumentParser:
+    parser = ArgumentParser(
+        prog="python main.py",
+        description="Runs a full benchmark with given models.",
+    )
+    parser.add_argument(
+        "-m",
+        "--models",
+        nargs="+",
+        required=True,
+        help="paths to the model zip archives",
+    )
+    parser.add_argument(
+        "-o", "--output", required=True, help="path to the output directory"
+    )
+    parser.add_argument(
+        "--fast-dev-run",
+        action="store_true",
+        help="run the benchmarks in fast-dev-run mode",
+    )
+    return parser
+
+
+# TODO: We should probably handle this in a different (nicer) way
+def _model_class_from_name(model_name: str) -> type[MLIPNetwork]:
+    if "visnet" in model_name:
+        return Visnet
+    if "mace" in model_name:
+        return Mace
+    if "nequip" in model_name:
+        return Nequip
+    raise NotImplementedError(
+        "Name of model zip archive does not contain info about the type of MLIP model."
+    )
+
+
+def main():
+    """Main for the MLIPAudit benchmark."""
+    args = _parser().parse_args()
+    output_dir = Path(args.output)
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s][%(name)s][%(levelname)s] - %(message)s",
+        force=True,
+    )
+    logger.setLevel(logging.INFO)
+
+    for model in args.models:
+        model_name = Path(model).stem
+        logger.info("Running benchmark with model %s.", model_name)
+
+        model_class = _model_class_from_name(model_name)
+        force_field = load_model_from_zip(model_class, model)
+
+        results = {}
+        for benchmark_class in BENCHMARKS:
+            benchmark = benchmark_class(
+                force_field=force_field, fast_dev_run=args.fast_dev_run
+            )
+            benchmark.run_model()
+            result = benchmark.analyze()
+            results[benchmark.name] = result
+
+        write_benchmark_results_to_disk(results, output_dir / model_name)
+        logger.info(
+            "Wrote benchmark results to disk at path %s.", output_dir / model_name
+        )
--- src/mlipaudit/small_molecule_conformer_selection/__init__.py
+++ src/mlipaudit/small_molecule_conformer_selection/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import importlib.metadata
-
-__version__ = importlib.metadata.version("mlipaudit")
+from mlipaudit.small_molecule_conformer_selection.conformer_selection import (
+    ConformerSelectionBenchmark,
+)
