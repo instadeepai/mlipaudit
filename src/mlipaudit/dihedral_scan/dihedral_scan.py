e86887fd0a9d0c91ec7779137097202133aeae18
Lucien Walewski <l.walewski@instadeep.com>
2025-07-30 17:00:20 +0200

    feat: add `run_model()` method for dihedral_scan.py

--- src/mlipaudit/dihedral_scan/dihedral_scan.py
+++ src/mlipaudit/dihedral_scan/dihedral_scan.py
@@ -2,10 +2,13 @@
 
 import functools
 import logging
+from collections import defaultdict
 
+from ase import Atoms
+from mlip.inference import run_batched_inference
 from pydantic import BaseModel, TypeAdapter
 
-from mlipaudit.benchmark import Benchmark, BenchmarkResult
+from mlipaudit.benchmark import Benchmark, BenchmarkResult, ModelOutput
 
 logger = logging.getLogger("mlipaudit")
 
@@ -25,6 +28,29 @@ class Fragment(BaseModel):
 Fragments = TypeAdapter(dict[str, Fragment])
 
 
+class FragmentModelOutput(BaseModel):
+    """Stores energy predictions per conformer.
+
+    Attributes:
+        fragment_name: The name of the fragment.
+        energy_predictions: The list of energy predictions
+            for each conformer of the fragment.
+    """
+
+    fragment_name: str
+    energy_predictions: list[float]
+
+
+class DihedralScanModelOutput(ModelOutput):
+    """Stores energy predictions per fragment per conformer.
+
+    Attributes:
+        fragments: A list of predictions per fragment.
+    """
+
+    fragments: list[FragmentModelOutput]
+
+
 class DihedralScanResult(BenchmarkResult):
     """Results object for the dihedral scan benchmark."""
 
@@ -38,12 +64,38 @@ class DihedralScanBenchmark(Benchmark):
     result_class = DihedralScanResult
 
     def run_model(self) -> None:
-        """Run a single point energy calculation for each structure.
+        """Run a single point energy calculation for each conformer for each fragment.
 
         The calculation is performed as a batched inference using the MLIP force field
-        directly. The energy profile is stored in the `model_output` attribute.
+        directly. The predicted energies are then stored as a list corresponding to
+        the conformers for each fragment.
         """
-        pass
+        atoms_list_all_structures = []
+        structure_indices_map = defaultdict(list)
+
+        index = 0
+
+        for fragment_name, fragment in self._torsion_net_500.items():
+            for conf_coord in fragment.conformer_coordinates:
+                atoms = Atoms(symbols=fragment.atom_symbols, positions=conf_coord)
+                atoms_list_all_structures.append(atoms)
+                structure_indices_map[fragment_name].append(index)
+                index += 1
+
+        predictions = run_batched_inference(
+            atoms_list_all_structures, self.force_field, batch_size=16
+        )
+
+        fragment_outputs = []
+
+        for fragment_name, indices in structure_indices_map.items():
+            fragment_output = FragmentModelOutput(
+                fragment_name=fragment_name,
+                energy_predictions=[predictions[i].energy for i in indices],
+            )
+            fragment_outputs.append(fragment_output)
+
+        self.model_output = DihedralScanModelOutput(fragments=fragment_outputs)
 
     def analyze(self) -> DihedralScanResult:
         """Calculates the RMSD between the MLIP and reference structures.
@@ -55,7 +107,7 @@ class DihedralScanBenchmark(Benchmark):
         raise NotImplementedError
 
     @functools.cached_property
-    def _torsion_net_500(self) -> list[Fragment]:
+    def _torsion_net_500(self) -> dict[str, Fragment]:
         with open(
             self.data_input_dir / self.name / TORSIONNET_DATASET_FILENAME,
             mode="r",
