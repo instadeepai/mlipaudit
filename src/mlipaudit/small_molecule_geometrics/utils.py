4ad5c29a28106cb3d5137c2a43fa1b6368222bf3
lwalew <l.walewski@instadeep.com>
2025-08-06 12:31:55 +0200

    feat: add create_mdtraj from simulation state

--- src/mlipaudit/small_molecule_geometrics/utils.py
+++ src/mlipaudit/small_molecule_geometrics/utils.py
@@ -1,3 +1,5 @@
+import mdtraj as md
+import numpy as np
 from mlip.simulation import SimulationState
 from pydantic import BaseModel, ConfigDict, TypeAdapter
 
@@ -27,3 +29,35 @@ class MoleculeSimulationOutput(BaseModel):
 
     molecule_name: str
     simulation_state: SimulationState
+
+
+def create_mdtraj_from_simulation_state(
+    simulation_state_positions: np.ndarray, atom_names: list[str]
+) -> md.Trajectory:
+    """Load a simulation state into an MDTraj trajectory.
+
+    Args:
+        simulation_state_positions: Atomic positions from a simulation state.
+        atom_names: list of atom names.
+
+    Returns:
+        trajectory: MDTraj trajectory.
+    """
+    topology = md.Topology()
+    chain = topology.add_chain()
+    residue = topology.add_residue("MOL", chain)
+
+    for name in atom_names:
+        topology.add_atom(
+            name=name, element=md.element.get_by_symbol(name), residue=residue
+        )
+
+    if simulation_state_positions.ndim == 2:
+        positions = simulation_state_positions.reshape(1, -1, 3)
+    else:
+        positions = simulation_state_positions
+
+    positions = positions / 10.0  # convert to nanometers
+    trajectory = md.Trajectory(topology=topology, xyz=positions)
+
+    return trajectory
