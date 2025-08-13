41f11dc175e2976321a3a84112150d9d87c38c03
lwalew <l.walewski@instadeep.com>
2025-08-13 16:51:54 +0200

    chore: linting

--- src/mlipaudit/water_radial_distribution/__init__.py
+++ src/mlipaudit/water_radial_distribution/__init__.py
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from mlipaudit.water_radial_distribution.water_radial_distribution import WaterRadialDistributionBenchmark
+from mlipaudit.water_radial_distribution.water_radial_distribution import (
+    WaterRadialDistributionBenchmark,
+)
