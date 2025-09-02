74e4754267aefd8001196de69bf3091b79e2f260
lwalew <l.walewski@instadeep.com>
2025-09-02 15:50:52 +0200

    chore: linting

--- src/mlipaudit/stability/__init__.py
+++ src/mlipaudit/stability/__init__.py
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from mlipaudit.stability.stability import StabilityResult, StabilityBenchmark, StabilityModelOutput
+from mlipaudit.stability.stability import (
+    StabilityBenchmark,
+    StabilityModelOutput,
+    StabilityResult,
+)
