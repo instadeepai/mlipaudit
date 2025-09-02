c48c0bc838d1f7b72c14c1c169783b6d54a70073
lwalew <l.walewski@instadeep.com>
2025-09-02 11:30:01 +0200

    chore: lint

--- src/mlipaudit/solvent_radial_distribution/__init__.py
+++ src/mlipaudit/solvent_radial_distribution/__init__.py
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from mlipaudit.solvent_radial_distribution.solvent_radial_distribution import SolventRadialDistributionResult, SolventRadialDistributionBenchmark, SolventRadialDistributionModelOutput
+from mlipaudit.solvent_radial_distribution.solvent_radial_distribution import (
+    SolventRadialDistributionBenchmark,
+    SolventRadialDistributionModelOutput,
+    SolventRadialDistributionResult,
+)
