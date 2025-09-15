0fded5d0ea822854e16707cbb9ba9f8591cd5254
lwalew <l.walewski@instadeep.com>
2025-09-15 12:57:21 +0200

    feat: don't skip nci if missing species

--- src/mlipaudit/noncovalent_interactions/noncovalent_interactions.py
+++ src/mlipaudit/noncovalent_interactions/noncovalent_interactions.py
@@ -305,6 +305,7 @@ class NoncovalentInteractionsBenchmark(Benchmark):
         "F",
         "B",
     }
+    skip_if_missing_species = False
 
     def run_model(self) -> None:
         """Run a single point energy calculation for each structure.
