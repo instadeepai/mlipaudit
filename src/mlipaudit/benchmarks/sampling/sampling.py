80efcf39cee2bfc501e89d04b76b952a02cb7396
lwalew <l.walewski@instadeep.com>
2025-11-17 13:42:03 +0100

    feat: update num steps for sampling

--- src/mlipaudit/benchmarks/sampling/sampling.py
+++ src/mlipaudit/benchmarks/sampling/sampling.py
@@ -51,9 +51,9 @@ BOX_SIZES = {
 }
 
 SIMULATION_CONFIG = {
-    "num_steps": 1_000,
-    "snapshot_interval": 10,
-    "num_episodes": 10,
+    "num_steps": 250_000,
+    "snapshot_interval": 10_000,
+    "num_episodes": 25,
     "temperature_kelvin": 300.0,
 }
 
